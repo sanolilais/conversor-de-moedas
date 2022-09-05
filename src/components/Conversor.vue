3ebab870855ad592996eaabbb73e192baa60644c
lais <sanolilais@gmail.com>
2022-09-05 09:31:51 -0300

    first commit - my first VueJS app

---
+++ src/components/Conversor.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="conversor">
+        <h2>{{moedaA}} Para {{moedaB}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Conversor",
+    props: ["moedaA" , "moedaB"]
+}
+</script>
+
+<style scoped>
+</style>
+
